3ba9e0301f756f5b0f542f097c90dee6a183ba47
palani731 <62838080+palani731@users.noreply.github.com>
2024-03-25 23:58:03 +0530

    Add waterman image

--- screens/LoginComponent.js
+++ screens/LoginComponent.js
@@ -1,57 +1,84 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import ContinueWithGoogleButton from "../components/ContinueWithGoogleButton";
 import StandardText from "../components/StandardText";
 import { Snackbar } from "react-native-paper";
+import { LinearGradient } from "expo-linear-gradient";
 
 const LoginScreen = () => {
-	const [snackbarVisible, setSnackbarVisible] = useState(true);
+    const [snackbarVisible, setSnackbarVisible] = useState(true);
 
-	return (
-		<View style={styles.container}>
-			<View style={styles.contentContainer}>
-				<ContinueWithGoogleButton
-					onPress={() => setSnackbarVisible((s) => !s)}
-				/>
-				<View style={styles.textContainer}>
-					<StandardText>Don't have an account?</StandardText>
-					<StandardText style={{ marginLeft: 50 }}>
-						Signup
-					</StandardText>
-				</View>
-			</View>
-			<Snackbar
-				visible={snackbarVisible}
-				style={{ backgroundColor: "green" }}
-				onDismiss={() => {
-					setSnackbarVisible(false);
-				}}
-				action={{
-					label: "Dismiss",
-				}}
-			>
-				Login successful
-			</Snackbar>
-		</View>
-	);
+    return (
+        <View style={styles.container}>
+            <StandardText style={styles.loginText}>Login</StandardText>
+            <Image
+                style={styles.watermanImage}
+                source={require("../assets/waterman.png")}
+            />
+            <LinearGradient
+                colors={[
+                    "rgba(119, 192, 241, 0.45)",
+                    "rgba(28, 163, 236, 0.45)",
+                    "rgba(0, 0, 0, 0.45)",
+                ]}
+                style={styles.contentContainer}
+            >
+                <ContinueWithGoogleButton
+                    onPress={() => setSnackbarVisible((s) => !s)}
+                />
+                <View style={styles.textContainer}>
+                    <StandardText>Don't have an account?</StandardText>
+                    <StandardText style={{ marginLeft: 50 }}>
+                        Signup
+                    </StandardText>
+                </View>
+            </LinearGradient>
+            <Snackbar
+                visible={snackbarVisible}
+                style={{ backgroundColor: "green" }}
+                onDismiss={() => {
+                    setSnackbarVisible(false);
+                }}
+                action={{
+                    label: "Dismiss",
+                }}
+            >
+                Login successful
+            </Snackbar>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-		backgroundColor: "#000",
-	},
-	contentContainer: {
-		alignItems: "center",
-		justifyContent: "center",
-		paddingHorizontal: 40,
-	},
-	textContainer: {
-		marginTop: 20,
-		flexDirection: "row",
-	},
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#000",
+    },
+    contentContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        paddingHorizontal: 40,
+        height: "40%",
+        borderRadius: 20,
+    },
+    loginText: {
+        marginRight: "auto",
+        paddingLeft: 40,
+        fontSize: 30,
+        fontWeight: "bold",
+        paddingBottom: "5%",
+    },
+    watermanImage: {
+        position: "absolute",
+        top: "18%",
+        right: "7%",
+    },
+    textContainer: {
+        marginTop: 20,
+        flexDirection: "row",
+    },
 });
 
 export default LoginScreen;
